2845c84eac2343977003c822c5176c9eacef0f66
noconei19 <102586076+noconei19@users.noreply.github.com>
2023-03-09 13:22:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,19 @@
+const url = "http://imiki.pl/cf/";
+const tableBody = document.querySelector("#dane tbody");
 
-const data = [
-  { name: 'Marcin', age: 30, city: 'Gliwice', sex:"M"  },
-  { name: 'Tomek', age: 25, city: 'Sosnowiec', sex:"M" },
-  { name: 'Jacek', age: 40, city: 'Bytom', sex:"M" },
-  { name: 'Kasia', age: 21, city: 'Bytom', sex:"K" },
-];
-
- url ="http://imiki.pl/cf/";
-
-axios.get(url)
-      .then(function(data) {
-        console.log("PERFECt.. all ok!");
-   console.log("number of apartements: " + data.length);
-        console.log(data);
-    })
-    .catch(function(err) {
-        console.log("ERR");
-        console.log(err);
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((row) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${row.timeStamp}</td>
+        <td>${row.chanel}</td>
+        <td>${row.servername}</td>
+        <td>${row.ver}</td>
+        <td>${row.data}</td>
+      `;
+      tableBody.appendChild(tr);
     });
-
-
-const table = document.createElement('table');
-
-table.setAttribute('class', 'tabelka');
-const headerRow = table.insertRow();
-
-Object.keys(data[0]).forEach(key => {
-  const headerCell = headerRow.insertCell();
-  headerCell.textContent = key;
-});
-
-
-data.forEach(obj => {
-  const row = table.insertRow();
-  Object.values(obj).forEach(value => {
-    const cell = row.insertCell();
-    cell.textContent = value;
-  });
-});
-
-const json = JSON.stringify(data);
-//console.log(json);
-
-document.body.appendChild(table);
+  })
+  .catch((error) => console.error(error));
